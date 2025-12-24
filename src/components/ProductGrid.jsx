13ff54c39558e0392b8cea04dfcc84a1200f7455
Yasvindhini U <yasvindhini@gmail.com>
2025-12-24 03:17:17 -0500

    `Updated ProductGrid component with new image imports and product data`

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -1,47 +1,48 @@
 import React from "react";
 
-import Img1 from "../assets/images/home/shotsseven.webp";
-import Img2 from "../assets/images/home/shots120.webp";
-import Img3 from "../assets/images/home/shots12.webp";
-import Img4 from "../assets/images/home/twink.webp";
-import Img5 from "../assets/images/Logo_1.avif";
+import Image1 from "../assets/images/shop/image1.jpeg";
+import Img2 from "../assets/images/shop/image2.jpeg";
+import Img3 from "../assets/images/shop/image3.jpeg";
+import Img4 from "../assets/images/shop/image4.jpeg";
+import Img5 from "../assets/images/shop/image5.jpeg";
+import Img6 from "../assets/images/shop/image6.jpeg";
 
 const products = [
   {
     id: 1,
     className: "div1",
-    name: "Flower Pots",
-    image: Img1,
+    name: "Kids",
+    image: Image1,
   },
   {
     id: 2,
     className: "div2",
-    name: "Sparklers",
+    name: "Return Gifts",
     image: Img2,
   },
   {
     id: 3,
     className: "div3",
-    name: "Sparklers",
+    name: "Classic",
     image: Img3,
   },
   {
     id: 4,
     className: "div4",
-    name: "Sparklers",
+    name: "Calender",
     image: Img4,
   },
   {
     id: 5,
     className: "div5",
-    name: "Sparklers",
+    name: "Cartoon",
     image: Img5,
   },
   {
     id: 6,
     className: "div6",
-    name: "Sparklers",
-    image: Img5,
+    name: "PaperBoard",
+    image: Img6,
   },
 ];
 
